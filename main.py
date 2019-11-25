e7a27963d2b65d5c3056f42d9ce78e62b8a12e67
unknown <adrian.bathan@shaw.ca>
2019-11-25 15:35:14 -0700

    set up rendertemplate

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Hello World!'
+    return render_template("main.html")
+
+	
+@app.route('/login')
+def login():
+	return "<H1> LOGIN </H1>"
+	
+	
+if __name__ == '__main__':
+	app.run(debug=True)
